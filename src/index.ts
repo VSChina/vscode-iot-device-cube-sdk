3dfb4cb72c2b8a052d9ecceb144f9dcced053cad
Zhe Li <zlhe@microsoft.com>
2019-05-16 20:42:45 +0800

    use stream

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 import { EventEmitter } from 'events';
 
 interface DeviceInfo {
@@ -16,26 +18,69 @@ interface Volume {
 
 export class FileSystem {
   static async listVolume() {
-    return await vscode.commands.executeCommand('iotcube.fsListVolume') as Volume[];
+    return (await vscode.commands.executeCommand(
+      'iotcube.fsListVolume'
+    )) as Volume[];
   }
 
   static async copyFile(sourcePath: string, targetPath: string) {
-    return await vscode.commands.executeCommand('iotcube.fsCopyFile', sourcePath, targetPath) as void;
+    return (await vscode.commands.executeCommand(
+      'iotcube.fsCopyFile',
+      sourcePath,
+      targetPath
+    )) as void;
+  }
+
+  static async transferFile(remotePath: string, localPath: string) {
+    return new Promise(
+      async (
+        resolve: (value: void) => void,
+        reject: (reason: Error) => void
+      ) => {
+        localPath = path.join(localPath, path.basename(remotePath));
+        const transferFileCallbackName = (await vscode.commands.executeCommand(
+          'iotcube.fsTransferFile',
+          localPath
+        )) as string;
+        const readStream = fs.createReadStream(remotePath, 'base64');
+        readStream.on('data', (chunk: string) => {
+          vscode.commands.executeCommand(transferFileCallbackName, chunk);
+        });
+        readStream.on('close', () => {
+          readStream.destroy();
+          vscode.commands.executeCommand(transferFileCallbackName, 'EOF');
+          resolve();
+        });
+        readStream.on('error', (error: Error) => {
+          reject(error);
+        });
+      }
+    );
   }
 }
 
 export class SSH {
   static async discover() {
-    return await vscode.commands.executeCommand('iotcube.sshDiscover') as DeviceInfo[];
+    return (await vscode.commands.executeCommand(
+      'iotcube.sshDiscover'
+    )) as DeviceInfo[];
   }
 
-  private _id: number|null = null;
+  private _id: number | null = null;
 
   async open(host: string, port: number, username: string, password: string) {
     if (this._id !== null) {
-      throw new Error('You must close current connection before open a new one.');
+      throw new Error(
+        'You must close current connection before open a new one.'
+      );
     }
-    this._id = await vscode.commands.executeCommand('iotcube.sshOpen', host, port, username, password) as number;
+    this._id = (await vscode.commands.executeCommand(
+      'iotcube.sshOpen',
+      host,
+      port,
+      username,
+      password
+    )) as number;
   }
 
   async close() {
@@ -47,39 +92,73 @@ export class SSH {
 
   spawn(command: string) {
     if (this._id === null) {
-      throw new Error('You must open an SSH connection before execute commands.');
+      throw new Error(
+        'You must open an SSH connection before execute commands.'
+      );
     }
     const event = new EventEmitter();
-    const spawnCallbackCommandName = `iotcubesdk.spawn${new Date().getTime()}_${Math.round(Math.random() * 100)}`;
-    const spawnCallback = vscode.commands.registerCommand(spawnCallbackCommandName, (eventName: string, payload?: string|Error) => {
-      event.emit(eventName, payload);
-      if (eventName === 'error' || eventName === 'close') {
-        spawnCallback.dispose();
+    const spawnCallbackCommandName = `iotcubesdk.spawn${new Date().getTime()}_${Math.round(
+      Math.random() * 100
+    )}`;
+    const spawnCallback = vscode.commands.registerCommand(
+      spawnCallbackCommandName,
+      (eventName: string, payload?: string | Error) => {
+        event.emit(eventName, payload);
+        if (eventName === 'error' || eventName === 'close') {
+          spawnCallback.dispose();
+        }
       }
-    });
+    );
 
-    vscode.commands.executeCommand('iotcube.sshSpawn', this._id, command, spawnCallbackCommandName);
+    vscode.commands.executeCommand(
+      'iotcube.sshSpawn',
+      this._id,
+      command,
+      spawnCallbackCommandName
+    );
     return event;
   }
 
   async exec(command: string) {
     if (this._id === null) {
-      throw new Error('You must open an SSH connection before execute commands.');
+      throw new Error(
+        'You must open an SSH connection before execute commands.'
+      );
     }
-    return await vscode.commands.executeCommand('iotcube.sshExec', this._id, command) as string;
+    return (await vscode.commands.executeCommand(
+      'iotcube.sshExec',
+      this._id,
+      command
+    )) as string;
   }
 
   async uploadFile(localPath: string, remotePath: string) {
     if (this._id === null) {
       throw new Error('You must open an SSH connection before upload files.');
     }
-    return await vscode.commands.executeCommand('iotcube.sshUploadFile', this._id, localPath, remotePath) as void;
+    const tempFolder = (await vscode.commands.executeCommand(
+      'iotcube.fsGetTempDir'
+    )) as string;
+    await FileSystem.transferFile(localPath, tempFolder);
+    const tempFilePath = path.join(tempFolder, path.basename(localPath));
+
+    return (await vscode.commands.executeCommand(
+      'iotcube.sshUploadFile',
+      this._id,
+      tempFilePath,
+      remotePath
+    )) as void;
   }
 
   async uploadFolder(localFolderPath: string, remoteFolderPath: string) {
     if (this._id === null) {
       throw new Error('You must open an SSH connection before upload files.');
     }
-    return await vscode.commands.executeCommand('iotcube.sshUploadFolder', this._id, localFolderPath, remoteFolderPath) as void;
+    return (await vscode.commands.executeCommand(
+      'iotcube.sshUploadFolder',
+      this._id,
+      localFolderPath,
+      remoteFolderPath
+    )) as void;
   }
 }
